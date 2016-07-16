506a6def3e30c6dc2a07b57d5bdcf15d2c71db2f
ebridge2 <ebridge2@jhu.edu>
2016-07-16 18:19:14 -0400

    conversion from npz to rds files

---
+++ ndmg/scripts/npz2rds.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# call from a directory where you want
+# to have your npz files converted to rds files.
+# written by eric bridgeford
+
+cd $(dirname "$0")
+
+echo "$(pwd)"
+
+while read -r npzline; do
+    ./npz2csv.py $npzline ${npzline//.npz/.csv}
+    ./csv2rds.R ${npzline//.npz/.csv} ${npzline//.npz/.rds}
+done < <(find $1 -name "*.npz")
+
+cd -
+echo "$(pwd)"
